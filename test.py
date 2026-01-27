67b136dba18e3fb99758f6e14c03e83c2e252091
Vincent <158262786+vdolera@users.noreply.github.com>
2026-01-27 10:52:59 +0800

    Heh

---
+++ test.py
@@ -0,0 +1,29 @@
+#money = 500
+#game = 1000
+
+#if money >= game:
+  #  print("Buy!!")
+#else:
+#    print("Sadge")
+#    missing = game - money
+#    print("I need this money", missing)
+
+#numbers = [1, 4, 6, 8 ,2]
+
+#print("List is ", numbers)
+
+#numbers.sort()
+
+#print(numbers)
+
+#for i in range(len(numbers)):
+#    for j in range(0, len(numbers) - i - 1):
+#        if numbers[j] > numbers[j + 1]:
+#            temp = numbers[j]
+#            numbers[j] = numbers[j + 1]
+#            numbers[j + 1] = temp
+
+#print("Sorted ", numbers)
+
+for i in range(0, 21, 5):
+    print(i)
